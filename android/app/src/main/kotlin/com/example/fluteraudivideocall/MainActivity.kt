e962c3cbfaf58e306773313a9b512747621010ed
CODING IS LIFE <84148157+codeislife07@users.noreply.github.com>
2023-03-05 16:18:57 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fluteraudivideocall/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluteraudivideocall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Consta.dart
@@ -0,0 +1,2 @@
+ var AppId='you app id';
+ var AppSign='app sign key';
